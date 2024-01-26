9ebb4d1484c9928b7b64a73954437ad9a7446f2c
vanttkthp <congvan2357@gmail.com>
2024-01-26 21:31:21 +0700

    update

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -8,10 +8,59 @@ import slider3 from "../../assets/images/slider3.webp";
 import slider4 from "../../assets/images/slider4.png";
 
 import { Col, Row } from "antd";
-import { Typography } from 'antd';
+import { Typography } from "antd";
 import CardComponent from "../../components/CardComponent/CardComponent";
 
+import { DownOutlined, SmileOutlined } from "@ant-design/icons";
+import { Dropdown, Space } from "antd";
+
 const { Title } = Typography;
+const items = [
+  {
+    key: "1",
+    label: (
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.antgroup.com"
+      >
+        1st menu item
+      </a>
+    ),
+  },
+  {
+    key: "2",
+    label: (
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.aliyun.com"
+      >
+        2nd menu item (disabled)
+      </a>
+    ),
+    icon: <SmileOutlined />,
+    disabled: true,
+  },
+  {
+    key: "3",
+    label: (
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.luohanacademy.com"
+      >
+        3rd menu item (disabled)
+      </a>
+    ),
+    disabled: true,
+  },
+  {
+    key: "4",
+    danger: true,
+    label: "a danger item",
+  },
+];
 
 export const HomePage = () => {
   const arr = [
@@ -26,7 +75,20 @@ export const HomePage = () => {
       <div style={{ padding: "0 120px" }}>
         <WrapperTypeProduct>
           {arr.map((item) => {
-            return <TypeProduct name={item} key={item} />;
+            return (
+              <Dropdown
+                menu={{
+                  items,
+                }}
+              >
+                <a onClick={(e) => e.preventDefault()}>
+                  <Space>
+                    <TypeProduct name={item}/>
+                    <DownOutlined />
+                  </Space>
+                </a>
+              </Dropdown>
+            );
           })}
         </WrapperTypeProduct>
       </div>
@@ -41,11 +103,13 @@ export const HomePage = () => {
         style={{ backgroundColor: "#fff3e1", padding: "0px 120px" }}
       >
         <Row>
-        <Title level={4} style={{marginTop:"10px"}}>Most favorable products</Title>
+          <Title level={4} style={{ marginTop: "10px" }}>
+            Most favorable products
+          </Title>
         </Row>
         <Row>
-          <div style={{display: 'flex', gap: '20px'}}>
-          <CardComponent />
+          <div style={{ display: "flex", gap: "20px" }}>
+            <CardComponent />
           </div>
         </Row>
       </div>
